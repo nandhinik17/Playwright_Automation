92bae17d9058b9a7f77ca86aeee4db88f8c8f603
nandhinik17 <your-email@example.com>
2026-02-01 22:27:14 +0530

    "Conf Change"

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -1,5 +1,9 @@
-// const test = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
 
-test('UI Basics Test', function(){
-
+test.only('UI Basics Test', async({page})=>{
+    // const context = await browser.newContext();
+    // const page = await context.newPage();
+    await page.goto('https://playwright.dev/');
+    console.log("Page Title: " + await page.title());
+    await expect(page).toHaveTitle('Fast and reliable end-to-end testing for modern web apps | Playwright');
 });
